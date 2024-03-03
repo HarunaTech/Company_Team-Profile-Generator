a884d6dd8c38ae2d4f38f9a34f8ccb2504350f47
HarunaTech <hamodharuna@gmail.com>
2024-03-04 00:31:40 +0200

    Created code for employee class

--- starter/lib/Employee.js
+++ starter/lib/Employee.js
@@ -1,1 +1,21 @@
 // TODO: Write code to define and export the Employee class
+class Employee {
+    constructor (name, id, email){
+        this.name = name;
+        this.id = id;
+        this.email = email;
+    }  // how to add item to our class
+    getName(){
+        return this.name
+    }
+    getId(){
+        return this.id
+    }
+    getEmail(){
+        return this.email
+    }
+    getRole(){
+        return "Employee"
+    }
+}  
+module.exports = Employee 
